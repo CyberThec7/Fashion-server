2f0bdffeb99b2b728953e7050e646b78782260f3
Wahab-Al <dev70tec@gmail.com>
2026-02-05 21:37:23 +0100

    add .env variables

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import { config } from "../config/env.js"
 
 const PORT = config.port
 
-app.listen(PORT, ()=> {
+app.listen(PORT,'0.0.0.0', ()=> {
   console.log('===============================')
   console.log(` Server is running on ${PORT}`)
   console.log('===============================')
